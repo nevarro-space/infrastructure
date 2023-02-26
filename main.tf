5bf49ee01a434a5eb5af6c9791d9d85c978751d4
Sumner Evans <me@sumnerevans.com>
2023-02-26 14:23:08 -0700

    terraform firewall
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- main.tf
+++ main.tf
@@ -33,3 +33,51 @@ resource "hcloud_ssh_key" "scarif_ssh_key" {
   name       = "sumner@scarif"
   public_key = file("./ssh-pubkeys/scarif.pub")
 }
+
+# Firewalls
+resource "hcloud_firewall" "web_server_firewall" {
+  name = "web-server"
+
+  rule {
+    description = "ping"
+    direction   = "in"
+    protocol    = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    description = "SSH"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    description = "HTTP"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    description = "HTTPS"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
