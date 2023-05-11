6f8da53828ba2f1835188b557dee27d7a6fcde23
Sumner Evans <me@sumnerevans.com>
2023-05-11 14:43:46 -0600

    matrix: mount postgres volume
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- nixos/hosts/matrix/default.nix
+++ nixos/hosts/matrix/default.nix
@@ -25,6 +25,7 @@
     checkId = "d7eae3e9-de4c-452b-9142-60e7831874c3";
     disks = [
       { path = "/"; threshold = 95; checkId = "5406129d-1352-4a24-b2db-6daa0c0a3d8f"; }
+      { path = "/mnt/postgresql-data"; threshold = 95; checkId = "2b04d70f-f432-4359-aab3-f0d2ba6d8995"; }
     ];
   };
 }
--- nixos/hosts/matrix/hardware-configuration.nix
+++ nixos/hosts/matrix/hardware-configuration.nix
@@ -1,8 +1,14 @@
-{ modulesPath, ... }: {
+{ modulesPath, terraform-outputs, ... }: {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/cf27e80b-f418-472e-8846-36073a76a628";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/cf27e80b-f418-472e-8846-36073a76a628";
+      fsType = "ext4";
+    };
+    "/mnt/postgresql-data" = {
+      device = terraform-outputs.matrix_server_postgresql_data_linux_device.value;
+      fsType = "ext4";
+    };
   };
 }
