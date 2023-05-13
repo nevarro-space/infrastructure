54d44167fabd2c837203bd59c6f8881b4b5389d3
Sumner Evans <me@sumnerevans.com>
2023-05-13 09:46:24 -0600

    shell: migrate to flake
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,39 @@
   description = "Nevarro Infrastructure NixOS deployments";
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ nixpkgs-unstable, ... }: {
-    colmena = import ./nixos/colmena.nix (inputs // {
-      terraform-outputs = nixpkgs-unstable.lib.importJSON ./terraform-output.json;
-    });
-  };
+  outputs = inputs@{ nixpkgs-unstable, flake-utils, ... }:
+    {
+      colmena = import ./nixos/colmena.nix (inputs // {
+        terraform-outputs = nixpkgs-unstable.lib.importJSON ./terraform-output.json;
+      });
+    } // (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs-unstable { system = system; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              colmena
+              git-crypt
+              nodePackages.bash-language-server
+              openssl
+              pre-commit
+              rnix-lsp
+              sops
+
+              # Terraform + Linters
+              terraform
+              terraform-docs
+              terraform-lsp
+              tflint
+              tfsec
+            ];
+          };
+        }
+      ));
 }
