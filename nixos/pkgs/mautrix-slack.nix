55dec3f1c827d3d8dd77ea3d9d7257c8703ba50b
Sumner Evans <me@sumnerevans.com>
2023-06-16 17:42:08 -0600

    mautrix-slack: upgrade
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- nixos/pkgs/mautrix-slack.nix
+++ nixos/pkgs/mautrix-slack.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "mautrix-slack";
-  version = "unstable-2023-06-02";
+  version = "unstable-2023-06-16";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "slack";
-    rev = "7e379049f1c0be2bb6d0b38eaff331f2eb000591";
-    sha256 = "sha256-BZFAaQkUURM2+X+KztEHeNKckJT6jKvd+TA9VXh1oMo=";
+    rev = "e9074423a08f7bbb9f489fae9a3d0d64c5d56c1c";
+    sha256 = "sha256-8Goz2LzIP5e3NluPI676C4GKUrTWBNnp+VB3o/wQt90=";
   };
 
   buildInputs = [ olm ];
 
-  vendorSha256 = "sha256-zLD06I6pE/BQss8C7WH2F5fid4npaB36mxfr8WitGFQ=";
+  vendorSha256 = "sha256-rG5z2UqfQgG+c/OF3mTLvX6O5RhouUgP9++5CA6YCw4=";
 
   meta = with lib; {
     homepage = "https://github.com/mautrix/slack";
