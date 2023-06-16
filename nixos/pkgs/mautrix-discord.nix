9fc25da2b79fa01fd5f21e731c942e3e7e29dcd9
Sumner Evans <me@sumnerevans.com>
2023-06-16 17:41:20 -0600

    mautrix-discord: upgrade
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- nixos/pkgs/mautrix-discord.nix
+++ nixos/pkgs/mautrix-discord.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "mautrix-discord";
-  version = "unstable-2023-06-02";
+  version = "unstable-2023-06-16";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "discord";
-    rev = "8c57b7a69bb4a7b3dfff5f0b48968569ac5ec65c";
-    sha256 = "sha256-S4ya7jzdQy4Q3o3ziNwYIpjzxo/vDEtH7iwnMHg+U9I=";
+    rev = "00465bb71509b984614ed41fa4f147a9d6bd4b3a";
+    sha256 = "sha256-2eYkYEoWo91ndKy7lI8JrjK7X7oPNLfws0XjpFvMICM=";
   };
 
   buildInputs = [ olm ];
 
-  vendorSha256 = "sha256-aw+6vTqwGyqCWpl6G/dVrGS/z+0CNUBhaBzz1QTxcbM=";
+  vendorSha256 = "sha256-wTJ2xcTFEU6+v5EQs+olKoGc9C5cktVOlnWP8CWMk4M=";
 
   meta = with lib; {
     homepage = "https://github.com/mautrix/discord";
