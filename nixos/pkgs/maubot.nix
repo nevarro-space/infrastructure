7a9cb1519d9de4d56b8b263d844b85a83f4496cf
Sumner Evans <me@sumnerevans.com>
2023-07-18 17:31:34 -0600

    maubot: remove frontend and just have Python module
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- nixos/pkgs/maubot.nix
+++ nixos/pkgs/maubot.nix
@@ -3,10 +3,6 @@
 , fetchPypi
 , fetchFromGitHub
 , python3
-, yarn
-, fixup_yarn_lock
-, nodejs
-, fetchYarnDeps
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "maubot";
@@ -48,35 +44,6 @@ python3.pkgs.buildPythonPackage rec {
     pycryptodome
     unpaddedbase64
   ];
-
-  nativeBuildInputs = [ fixup_yarn_lock yarn nodejs ];
-
-  offlineCache = fetchYarnDeps {
-    yarnLock = src + "/maubot/management/frontend/yarn.lock";
-    sha256 = "sha256-VBPZbtqF9u63yRgk0PObhUMvV8s7UXSs6nr87cPeLz4=";
-  };
-
-  configurePhase = ''
-    runHook preConfigure
-
-    export HOME=$PWD/tmp
-    mkdir -p $HOME
-
-    pushd maubot/management/frontend
-    fixup_yarn_lock yarn.lock
-    yarn config --offline set yarn-offline-mirror $offlineCache
-    yarn install --offline --frozen-lockfile --ignore-platform --ignore-scripts --no-progress --non-interactive
-    patchShebangs node_modules
-    popd
-
-    runHook postConfigure
-  '';
-
-  preBuild = ''
-    pushd maubot/management/frontend
-    yarn build
-    popd
-  '';
 
   postPatch = ''
     sed -i -e 's/aiosqlite>=0.16,<0.19/aiosqlite<0.20,>=0.16/' requirements.txt
