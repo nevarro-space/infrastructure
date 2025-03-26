b003ec1581d4193eb586d6a57494609dea19f464
Sumner Evans <me@sumnerevans.com>
2025-03-26 15:45:03 -0600

    matrix-synapse: 1.127.0 -> 1.127.1
    https://github.com/element-hq/synapse/releases/tag/v1.127.1
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- nixos/colmena.nix
+++ nixos/colmena.nix
@@ -15,13 +15,13 @@ in {
           matrix-synapse-unwrapped =
             super.matrix-synapse-unwrapped.overridePythonAttrs (old: rec {
               pname = "matrix-synapse";
-              version = "1.127.0";
+              version = "1.127.1";
 
               src = super.fetchFromGitHub {
                 owner = "element-hq";
                 repo = "synapse";
                 rev = "v${version}";
-                hash = "sha256-6eIb7LWq8lLyVGSoQi+0bbNmZZBGkyu2bA9JUg/vYmk=";
+                hash = "sha256-DNUKbb+d3BBp8guas6apQ4yFeXCc0Ilijtbt1hZkap4=";
               };
 
               cargoDeps = super.rustPlatform.fetchCargoVendor {
