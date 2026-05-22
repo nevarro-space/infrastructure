67aee7b1cf1c7f512ec00ee7ee86b3fcc3d8e942
Sumner Evans <me@sumnerevans.com>
2026-05-22 11:13:57 -0600

    alloy: fix hostname
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- nixos/modules/services/alloy.nix
+++ nixos/modules/services/alloy.nix
@@ -1,4 +1,31 @@
+{ config, ... }:
 {
   services.alloy.enable = true;
-  environment.etc."alloy/config.alloy".source = ./alloy.conf;
+  environment.etc."alloy/config.alloy".text = ''
+    discovery.relabel "journal" {
+    	targets = []
+
+    	rule {
+    		source_labels = ["__journal__systemd_unit"]
+    		target_label  = "unit"
+    	}
+    }
+
+    loki.source.journal "journal" {
+    	max_age       = "12h0m0s"
+    	relabel_rules = discovery.relabel.journal.rules
+    	forward_to    = [loki.write.default.receiver]
+    	labels        = {
+    		host = "${config.networking.hostName}",
+    		job  = "systemd-journal",
+    	}
+    }
+
+    loki.write "default" {
+    	endpoint {
+    		url = "http://10.0.1.2:3100/loki/api/v1/push"
+    	}
+    	external_labels = {}
+    }
+  '';
 }
